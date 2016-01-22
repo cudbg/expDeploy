4eddbe13ebe4e0a3216edfd05f4ce845d5e21b80
jssands <js4597@columbia.edu>
2016-01-22 09:05:28 -0500

    planout working

--- src/expdeploy/settings.py
+++ src/expdeploy/settings.py
@@ -117,3 +117,7 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "expdeploy/static"),
+]
