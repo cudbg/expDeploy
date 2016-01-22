4c81d34bde484beae10b7f0412ed41154cd80c8b
Hamed Nilforoshan <hamed@nilforoshan.com>
2016-01-22 03:50:34 -0500

    Extended to upload static files as well

--- src/expdeploy/fileloader/views.py
+++ src/expdeploy/fileloader/views.py
@@ -7,16 +7,26 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 from expdeploy.fileloader.models import Document
-from expdeploy.fileloader.forms import DocumentForm
 
+from expdeploy.fileloader.forms import DocumentForm
+from expdeploy.fileloader.forms import AttachmentForm
 
 def list(request):
     # Handle file upload
     if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
+       # print(request.FILES);
+        form = DocumentForm(request.POST, request.FILES);
+        form2 = AttachmentForm(request.POST, request.FILES)
         if form.is_valid():
             newdoc = Document(docfile=request.FILES['docfile'])
             newdoc.save()
+          #  print(request.FILES);
+
+            for each in request.FILES.getlist("attachments"):
+                newdoc = Document(docfile=each);
+                newdoc.save();
+                print(each);
+                #return super(UploadView, self).form_valid(form)
 
             # Redirect to the document list after POST
             return HttpResponseRedirect(reverse('expdeploy.fileloader.views.list'))
@@ -25,10 +35,10 @@ def list(request):
 
     # Load documents for the list page
     documents = Document.objects.all()
-
+    attachments = AttachmentForm();
     # Render list page with the documents and the form
     return render_to_response('list.html',
-        {'documents': documents, 'form': form},
+        {'documents': documents, 'form': form, 'attachments':attachments},
         context_instance=RequestContext(request)
     )
 
