34020a908b348468c27a17975fc8bd4259af0800
jssands <js4597@columbia.edu>
2016-02-12 21:09:19 -0500

    settings thing again

--- src/expdeploy/settings.py
+++ src/expdeploy/settings.py
@@ -44,15 +44,22 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-   #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'expdeploy.api',
+
+   #  'django.middleware.security.SecurityMiddleware',
+   #  'django.contrib.sessions.middleware.SessionMiddleware',
+   #  'django.middleware.common.CommonMiddleware',
+   # #'django.middleware.csrf.CsrfViewMiddleware',
+   #  'django.contrib.auth.middleware.AuthenticationMiddleware',
+   #  #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+   #  'django.contrib.messages.middleware.MessageMiddleware',
+   #  'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   #  #'expdeploy.api',
 )
 
 ROOT_URLCONF = 'expdeploy.urls'
