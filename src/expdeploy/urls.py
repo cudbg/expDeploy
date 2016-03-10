7b0153f724fca502af5db97155763fab8df2c75c
jssands <js4597@columbia.edu>
2016-03-10 14:10:48 -0500

    commit before changing testapp name.

--- src/expdeploy/urls.py
+++ src/expdeploy/urls.py
@@ -6,10 +6,10 @@ from django.views.generic import RedirectView
 from django.contrib import admin
 
 urlpatterns = [
+
+	url(r'^$', RedirectView.as_view(url='/testapp/', permanent=False)),
+	url(r'^admin/', include(admin.site.urls)),
 	url(r'^api/', include('expdeploy.api.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^$', RedirectView.as_view(url='/testapp/', permanent=False)),
-    url(r'^testapp/', include('expdeploy.testapp.urls')),
-
+	url(r'^testapp/', include('expdeploy.testapp.urls')),
+	
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
