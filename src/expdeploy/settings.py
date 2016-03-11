6c0b35e100630ff84fab4901fc5fb1d2599328ec
Hamed Nilforoshan <hamed@nilforoshan.com>
2016-03-11 05:26:30 -0500

    bleh

--- src/expdeploy/settings.py
+++ src/expdeploy/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
    #  'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
