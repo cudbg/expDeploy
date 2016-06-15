eb0917e90ce69b0c65907a281fcd033697a5b884
jssands <js4597@columbia.edu>
2016-06-15 15:57:57 -0400

    accidentally pushed wrong db config

--- src/expdeploy/settings.py
+++ src/expdeploy/settings.py
@@ -105,14 +105,14 @@ WSGI_APPLICATION = 'expdeploy.wsgi.application'
 
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': 'db.sqlite3',
-   # 	'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   # 	'NAME': 'gpaas',
-   # 	'USER': 'gpaasteam',
-   # 	'PASSWORD': 'gpaas',
-   # 	'HOST': 'localhost',
-   # 	'PORT': '',
+      # 'ENGINE': 'django.db.backends.sqlite3',
+      # 'NAME': 'db.sqlite3',
+     	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+     	'NAME': 'gpaas',
+     	'USER': 'gpaasteam',
+     	'PASSWORD': 'gpaas',
+     	'HOST': 'localhost',
+   	  'PORT': '',
 	   }
 }
 
