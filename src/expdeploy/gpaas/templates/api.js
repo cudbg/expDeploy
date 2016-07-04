df39f85b58838bc3a1686f93fe30ccd468e9c7db
Hamed Nilforoshan <hamed@nilforoshan.com>
2016-07-04 13:06:07 -0400

    catchErrors

--- src/expdeploy/gpaas/templates/api.js
+++ src/expdeploy/gpaas/templates/api.js
@@ -7426,6 +7426,12 @@ var gpaas = (function() {
 	//var serverurl = "https://192.241.179.74:8000"
 	//var serverurl = "https://localhost:8000"
 
+
+	var catchError = function(e) {
+		alert(e)
+		console.log("...caught error...")
+	}
+
 	var serverurl = "https://gpaas.xyz"
 
 	var getUrlParameter = function getUrlParameter(sParam) {
@@ -7472,8 +7478,13 @@ var gpaas = (function() {
 			task_id: currentId
 		}
 
-		console.log(JSON.stringify(postData));
-		xmlhttp.send(JSON.stringify(postData));
+		try {
+
+			console.log(JSON.stringify(postData));
+			xmlhttp.send(JSON.stringify(postData));
+		} catch (e) {
+			catchError(e)
+		}
 	}
 
 
@@ -7486,193 +7497,210 @@ var gpaas = (function() {
 		action = getUrlParameter("turkSubmitTo") + "/mturk/externalSubmit"
 		formString = '<form action="' + action + '"><input type="hidden" name="status" value="COMPLETE"><input type="hidden" name="assignmentId" value="' + assignmentIDFOrm + '"></form>';
 
-		$(formString).appendTo('body').submit();
-
+		try {
+			$(formString).appendTo('body').submit();
+		} catch (e) {
+			catchError(e)
+		}
 
 	}
 
 	var nextTask = function() {
 
-		clearTask();
+		try {
+			clearTask();
 
-		taskStart = Math.round(new Date().getTime() / 1000)
+			taskStart = Math.round(new Date().getTime() / 1000)
 
 
-		if (tasks.length == 0) {
+			if (tasks.length == 0) {
 
-			earned += bonusPay
-			console.log("No tasks left")
+				earned += bonusPay
+				console.log("No tasks left")
 
-			if (completed > 0) {
-				finish({ submit: submit, tasksCompleted: completed, moneyEarned: earned })
+				if (completed > 0) {
+					finish({ submit: submit, tasksCompleted: completed, moneyEarned: earned })
+				}
+
+				return;
 			}
 
-			return;
+			entry = tasks[0];
+			currentId = tasks[0]["identifier"];
+			earned = completed * perTaskPay
+			viewTask({ params: entry, logData: logData, nextTask: nextTask, tasksCompleted: completed, moneyEarned: earned })
+			tasks.shift();
+			completed++
+
+		} catch (e) {
+			catchError(e)
 		}
-
-		entry = tasks[0];
-		currentId = tasks[0]["identifier"];
-		earned = completed * perTaskPay
-		viewTask({ params: entry, logData: logData, nextTask: nextTask, tasksCompleted: completed, moneyEarned: earned })
-		tasks.shift();
-		completed++
-
 	}
 
 
 
 	var endTasks = function() {
-		clearTask()
-		console.log("TASK CLEARED")
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.open("GET", serverurl + "/api/finishTasks?researcher=" + researcher + "&experiment=" + n + "&task=" + task + "&wid=" + wid, false); // false for synchronous request
-		xmlHttp.send(null);
+		try {
+			clearTask()
+			console.log("TASK CLEARED")
+			var xmlHttp = new XMLHttpRequest();
+			xmlHttp.open("GET", serverurl + "/api/finishTasks?researcher=" + researcher + "&experiment=" + n + "&task=" + task + "&wid=" + wid, false); // false for synchronous request
+			xmlHttp.send(null);
 
-		submit()
+			submit()
+		} catch (e) {
+			catchError(e)
+		}
 	}
 
 
 	function setupExperiment(options) {
 
-		console.log(window.location)
-		n = "{{experiment}}"
-		researcher = "{{username}}"
 
-		task = options.task;
+		try {
 
+			console.log(window.location)
+			n = "{{experiment}}"
+			researcher = "{{username}}"
 
-
-		if (n.includes("{{experiment}")) {
-			n = options.name;
-			researcher = options.researcher;
-		}
-
-		viewTask = options.viewTask;
-		finish = options.finish;
-		clearTask = options.clearTask;
-		numberTasks = options.numTasks
+			task = options.task;
 
 
-		var sandboxParam = getUrlParameter("turkSubmitTo")
-		var sandbox = true
-		if  (""+sandboxParam != "undefined") {
-			sandbox =(getUrlParameter("turkSubmitTo").indexOf("sandbox")!=-1)
-		}
 
-
-		console.log("AM I IN SANDBOX MODE???" + sandbox)
-		var hitID = getUrlParameter("hitId")
-		var assignmentID = getUrlParameter("assignmentId")
-		wid = getUrlParameter("workerId")
-
-		if (local) {
-			wid = "exampleWorker"
-		}
-
-		if ("" + wid == "undefined") {
-			alert("You must accept the HIT in order to start!!!")
-			return
-		}
-
-		failed = false
-
-		options.qualificationTasks.forEach(function(entry) {
-
-			e = entry()
-
-			if (e == false) {
-				console.log("THIS IS WHERE I FAILED")
-					//console.log(entry)
-				console.log("BLEH")
-				failed = true
+			if (n.includes("{{experiment}")) {
+				n = options.name;
+				researcher = options.researcher;
 			}
 
-		});
+			viewTask = options.viewTask;
+			finish = options.finish;
+			clearTask = options.clearTask;
+			numberTasks = options.numTasks
 
-		if (failed) {
-			options.failQualification()
-		}
 
-		if (failed == false) {
+			var sandboxParam = getUrlParameter("turkSubmitTo")
+			var sandbox = true
+			if ("" + sandboxParam != "undefined") {
+				sandbox = (getUrlParameter("turkSubmitTo").indexOf("sandbox") != -1)
+			}
 
-			options.trainingTasks.forEach(function(entry) {
-				console.log(entry());
-				if (entry == false) {
-					return
-				}
 
-			});
-
+			console.log("AM I IN SANDBOX MODE???" + sandbox)
+			var hitID = getUrlParameter("hitId")
+			var assignmentID = getUrlParameter("assignmentId")
+			wid = getUrlParameter("workerId")
 
 			if (local) {
-				console.log("local setup of experiment")
+				wid = "exampleWorker"
+			}
 
+			if ("" + wid == "undefined") {
+				alert("You must accept the HIT in order to start!!!")
+				return
+			}
 
-				temp = options.params
+			failed = false
 
-				obj = { "params": [], "pay": 1.0, "bonus": 1.0 }
+			options.qualificationTasks.forEach(function(entry) {
 
-				console.log(n)
-				for (k = 0; k < numberTasks; k++) {
-					console.log(k)
+				e = entry()
 
-					trial = {}
-					temp["params"].forEach(function(entry) {
-
-						name = entry["name"]
-						choice = entry["options"][Math.floor(Math.random() * entry["options"].length)]
-						trial[name] = choice
-					});
-
-					obj["params"].push(trial)
+				if (e == false) {
+					console.log("THIS IS WHERE I FAILED")
+						//console.log(entry)
+					console.log("BLEH")
+					failed = true
 				}
 
-				completed = 0
-
-				perTaskPay = obj["pay"]
-				bonusPay = obj["bonus"]
-
-				console.log(obj)
-
-				obj["params"].forEach(function(entry) {
-
-					tasks.push(entry);
-
-				});
-
-			} else {
-
-				var xmlHttp = new XMLHttpRequest();
-				xmlHttp.open("GET", serverurl + "/api/task?researcher=" + researcher + "&experiment=" + n + "&task=" + task + "&wid=" + wid + "&n=" + numberTasks + "&hitId=" + hitID + "&assignmentId=" + assignmentID +"&isSandbox="+sandbox, false); // false for synchronous request
-				xmlHttp.send(null);
-				resp = xmlHttp.responseText.replaceAll("'", '"');
-
-				obj = JSON.parse(resp);
-				console.log(obj["params"]);
-
-				obj["params"].forEach(function(entry) {
-
-					tasks.push(entry);
-
-				});
-
-				perTaskPay = obj["pay"]
-				bonusPay = obj["bonus"]
-
-				completed = numberTasks - obj["params"].length
-				console.log('..........this is how many completed' + completed)
-
-
-			}
-		} else {
-			$.get(serverurl + "/api/ban?researcher=" + researcher + "&experiment=" + n + "&task=" + task + "&wid=" + wid, function(data) {
-				console.log("BANNED")
 			});
 
-			alert("Unfortunately, based on prior qualification results, no tasks can be assigned to your Worker ID. Please return the HIT to end.")
-		}
+			if (failed) {
+				options.failQualification()
+			}
 
-		//nextTask();
+			if (failed == false) {
+
+				options.trainingTasks.forEach(function(entry) {
+					console.log(entry());
+					if (entry == false) {
+						return
+					}
+
+				});
+
+
+				if (local) {
+					console.log("local setup of experiment")
+
+
+					temp = options.params
+
+					obj = { "params": [], "pay": 1.0, "bonus": 1.0 }
+
+					console.log(n)
+					for (k = 0; k < numberTasks; k++) {
+						console.log(k)
+
+						trial = {}
+						temp["params"].forEach(function(entry) {
+
+							name = entry["name"]
+							choice = entry["options"][Math.floor(Math.random() * entry["options"].length)]
+							trial[name] = choice
+						});
+
+						obj["params"].push(trial)
+					}
+
+					completed = 0
+
+					perTaskPay = obj["pay"]
+					bonusPay = obj["bonus"]
+
+					console.log(obj)
+
+					obj["params"].forEach(function(entry) {
+
+						tasks.push(entry);
+
+					});
+
+				} else {
+
+					var xmlHttp = new XMLHttpRequest();
+					xmlHttp.open("GET", serverurl + "/api/task?researcher=" + researcher + "&experiment=" + n + "&task=" + task + "&wid=" + wid + "&n=" + numberTasks + "&hitId=" + hitID + "&assignmentId=" + assignmentID + "&isSandbox=" + sandbox, false); // false for synchronous request
+					xmlHttp.send(null);
+					resp = xmlHttp.responseText.replaceAll("'", '"');
+
+					obj = JSON.parse(resp);
+					console.log(obj["params"]);
+
+					obj["params"].forEach(function(entry) {
+
+						tasks.push(entry);
+
+					});
+
+					perTaskPay = obj["pay"]
+					bonusPay = obj["bonus"]
+
+					completed = numberTasks - obj["params"].length
+					console.log('..........this is how many completed' + completed)
+
+
+				}
+			} else {
+				$.get(serverurl + "/api/ban?researcher=" + researcher + "&experiment=" + n + "&task=" + task + "&wid=" + wid, function(data) {
+					console.log("BANNED")
+				});
+
+				alert("Unfortunately, based on prior qualification results, no tasks can be assigned to your Worker ID. Please return the HIT to end.")
+			}
+
+			//nextTask();
+		} catch (e) {
+			catchError(e)
+		}
 
 
 	}
