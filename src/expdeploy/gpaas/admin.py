97967e4f99885cd32131608eaa529e4b4de7f130
Hamed Nilforoshan <hamed@nilforoshan.com>
2016-07-19 11:36:00 -0400

    update

--- src/expdeploy/gpaas/admin.py
+++ src/expdeploy/gpaas/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from expdeploy.gpaas.models import ExperimentFile
 from expdeploy.gpaas.models import ExperimentModel
+from expdeploy.gpaas.models import Researcher
 
 #register models
 
@@ -15,5 +16,13 @@ class ExperimentAdmin(admin.ModelAdmin):
 	class Meta:
 		model = ExperimentModel
 
+
+class ResearcherAdmin(admin.ModelAdmin):
+	list_display = ["aws_key_id", "aws_secret_key","user__name"]
+	class Meta:
+		model = ExperimentModel
+
+
 admin.site.register(ExperimentFile, FileAdmin)
 admin.site.register(ExperimentModel, ExperimentAdmin)
+admin.site.register(Researcher, ResearcherAdmin)
