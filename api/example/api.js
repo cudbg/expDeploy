9f43f0f76ac9a739650ab2ce858d7b87bc3d2213
Hamed Nilforoshan <hamed@nilforoshan.com>
2016-07-20 21:17:50 -0400

    fix

--- api/example/api.js
+++ api/example/api.js
@@ -7886,7 +7886,7 @@ var gpaas = (function() {
 							type: "GET",
 							url:  serverurl + "/api/task?researcher=" + researcher + "&experiment=" + n + "&task=" + task + "&wid=" + wid + "&n=" + numberTasks + "&hitId=" + hitID + "&assignmentId=" + assignmentID + "&isSandbox=" + sandbox,
 							success: function(data, status, jqXHR) {
-								obj = JSON.parse(data);
+								obj = JSON.parse(data.responseText.replaceAll("'", '"'););
 								obj["params"].forEach(function(entry) {
 
 									tasks.push(entry);
