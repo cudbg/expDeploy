50acc36ef0e9b1351cd7c677a5dcd8ad8e68ed56
sirrice <ewu@cs.columbia.edu>
2016-11-20 00:58:18 -0500

    changed requests to https

--- src/expdeploy/gpaas/templates/api.js
+++ src/expdeploy/gpaas/templates/api.js
@@ -7438,7 +7438,7 @@ var gpaas = (function() {
 		console.log("...caught error...")
 	}
 
-	var serverurl = "http://gpaas.xyz"
+	var serverurl = "https://gpaas.xyz"
 
 	var getUrlParameter = function getUrlParameter(sParam) {
 		var sPageURL = decodeURIComponent(window.location.search.substring(1)),
