0c3c2e23fe0cd1eb08c25bf355a708ce8180c8a4
Hamed Nilforoshan <hamed@nilforoshan.com>
2016-11-21 01:07:39 -0500

    not much

--- api/src/interface.js
+++ api/src/interface.js
@@ -49,7 +49,7 @@ var gpaas = (function() {
 		console.log("...caught error...")
 	}
 
-	var serverurl = "https://www.gpaas.xyz"
+	var serverurl = "https://www.gpaas.xyz" //URL to send all requests to
 
 	var getUrlParameter = function getUrlParameter(sParam) {
 		var sPageURL = decodeURIComponent(window.location.search.substring(1)),
